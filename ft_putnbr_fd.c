6874949f5d6a0c3c72b8f38eae931625ab534d7d
Ruddy LAVALLOIR <rlavallo@e2r8p19.42.fr>
2016-02-16 12:42:45 +0100

    Method now works

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: rlavallo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/21 08:52:22 by rlavallo          #+#    #+#             */
-/*   Updated: 2016/01/21 09:01:45 by rlavallo         ###   ########.fr       */
+/*   Updated: 2016/02/16 12:41:40 by rlavallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
-		ft_putstr("-214748648");
+		ft_putstr_fd("-2147483648", fd);
 	else
 	{
 		if (n < 0)
 		{
-			ft_putchar_fd('-', 1);
+			ft_putchar_fd('-', fd);
 			n = -n;
 		}
 		if (n >= 10)
 		{
-			ft_putnbr_fd(n / 10, 1);
-			ft_putnbr_fd(n % 10, 1);
+			ft_putnbr_fd(n / 10, fd);
+			ft_putnbr_fd(n % 10, fd);
 		}
 		else
 		{
